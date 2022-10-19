0dfabac564a6f63f93ff150fafb6bb36c07006c2
Maho Pacheco <mapach@microsoft.com>
2022-10-18 22:13:53 -0700

    All changes one PR

---
+++ Source/v3Net/MeetupBot/Controllers/NotificationsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.Web.Http;
+using global::MeetupBot.Helpers;
+
+namespace MeetupBot.Controllers
+{
+    public class NotificationsController : ApiController
+    {
+        // POST api/<welcome-users>/<key>
+        [Route("api/welcome-users/{teamId}")]
+        public void PostWelcomeUsers([FromUri] string teamId)
+        {
+            if (string.IsNullOrEmpty(teamId))
+            {
+                System.Diagnostics.Trace.TraceError($"Received Invalid TeamId. Do not do anything.");
+            }
+            else
+            {
+                HostingEnvironment.QueueBackgroundWorkItem(ct => WelcomeUsersAsync(teamId));
+                return;
+            }
+        }
+
+        private static async Task<int> WelcomeUsersAsync(string teamId)
+        {
+            return await MeetupBot.WelcomeAllUsersAsync(teamId);
+        }
+
+    }
+}

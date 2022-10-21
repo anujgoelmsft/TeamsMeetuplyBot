507bf43ffed7f0409ee69fc864cf5582cee63a7d
Maho Pacheco <mapach@microsoft.com>
2022-10-21 13:21:24 -0700

    Adding optin/optout and trigger welcome to the main page

--- Source/v3Net/TriggerPairingWebApp/Controllers/HomeController.cs
+++ Source/v3Net/TriggerPairingWebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Azure;
 using Newtonsoft.Json;
 using TriggerPairingWebApp.Models;
 
@@ -15,8 +16,13 @@ namespace TriggerPairingWebApp.Controllers
 		// GET: Home
 		public ActionResult Index()
 		{
-			var teamsInfoList = TeamsDataProvider.GetAllTeams();
-			var teamsSelectList = teamsInfoList.Select(t => new SelectListItem
+			List<TeamInfo> teamsInfoList;
+
+            teamsInfoList = TeamsDataProvider.GetAllTeams();
+
+            System.Diagnostics.Trace.TraceInformation($"{teamsInfoList.Count} teams retrieved.");
+
+            var teamsSelectList = teamsInfoList.Select(t => new SelectListItem
 			{
 				Text = t.Teamname,
 				Value = t.Id
@@ -33,15 +39,28 @@ namespace TriggerPairingWebApp.Controllers
 				})
 			};
 
-			return View(viewModel);
+            System.Diagnostics.Trace.TraceInformation($"Rendering view model");
+
+            return View(viewModel);
 		}
 
-		[HttpPost]
-		public ActionResult Index(TeamViewModel model)
+
+        [HttpPost]
+		public ActionResult Pair(TeamViewModel model)
 		{
-			// Send Web Request to trigger pairing
-			var selectedTeamId = model.SelectedTeamId;
-			WebRequest webRequest = WebRequest.Create($"https://meetupbotappservice.azurewebsites.net/api/processnow/{selectedTeamId}");
+			var baseUri = CloudConfigurationManager.GetSetting("MeetupBotAppUri") ?? "https://meetupbotappservice.azurewebsites.net";
+
+            // Send Web Request to trigger pairing
+            var selectedTeamId = model.SelectedTeamId;
+
+            if (selectedTeamId == null)
+            {
+                throw new ArgumentNullException("selectedTeamId");
+            }
+
+            System.Diagnostics.Trace.TraceInformation($"Creating a request for {baseUri}/api/processnow/{selectedTeamId}");
+            
+			WebRequest webRequest = WebRequest.Create($"{baseUri}/api/processnow/{selectedTeamId}");
 			webRequest.Method = "POST";
 			webRequest.ContentLength = 0;
 			webRequest.GetResponse();
@@ -50,7 +69,31 @@ namespace TriggerPairingWebApp.Controllers
 			return Redirect("~/");
 		}
 
-		public ActionResult About()
+        [HttpPost]
+        public ActionResult Welcome(TeamViewModel model)
+        {
+            var baseUri = CloudConfigurationManager.GetSetting("MeetupBotAppUri") ?? "https://meetupbotappservice.azurewebsites.net";
+
+            // Send Web Request to trigger pairing
+            var selectedTeamId = model.SelectedTeamId;
+
+            System.Diagnostics.Trace.TraceInformation($"Creating a request for {baseUri}/api/welcome-users/{selectedTeamId}");
+
+			if (selectedTeamId == null)
+			{
+				throw new ArgumentNullException("selectedTeamId");
+			}
+
+            WebRequest webRequest = WebRequest.Create($"{baseUri}/api/welcome-users/{selectedTeamId}");
+            webRequest.Method = "POST";
+            webRequest.ContentLength = 0;
+            webRequest.GetResponse();
+
+            // go back to home page
+            return Redirect("~/");
+        }
+
+        public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
 
--- Source/v3Net/TriggerPairingWebApp/Models/TeamsDataProvider.cs
+++ Source/v3Net/TriggerPairingWebApp/Models/TeamsDataProvider.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
+using Microsoft.Azure;
 using Newtonsoft.Json;
 
 namespace TriggerPairingWebApp.Models
@@ -12,10 +13,17 @@ namespace TriggerPairingWebApp.Models
 	{
         public static List<TeamInfo> GetAllTeams()
         {
+            var baseUri = CloudConfigurationManager.GetSetting("MeetupBotAppUri") ?? "https://meetupbotappservice.azurewebsites.net";
+
+            System.Diagnostics.Trace.TraceInformation($"Creating a request for {baseUri}/api/processnow");
+
             // get all 
-            WebRequest webRequest = WebRequest.Create($"https://meetupbotappservice.azurewebsites.net/api/processnow");
+            WebRequest webRequest = WebRequest.Create($"{baseUri}/api/processnow");
+
             webRequest.Method = "GET";
             var response = webRequest.GetResponse();
+
+            System.Diagnostics.Trace.TraceInformation($"Response: {JsonConvert.SerializeObject(response)}");
 
             using (StreamReader reader = new StreamReader(response.GetResponseStream()))
             {
